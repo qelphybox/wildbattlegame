d21b02e4a24e3139e9a88ee537dc90769d08e690
kirill_bobykin <qelphybox@gmail.com>
2016-05-03 23:05:28 +0300

    Добавлено логирование в файл

--- lib/wild_battle_game.rb
+++ lib/wild_battle_game.rb
@@ -20,18 +20,20 @@ module WildBattleGame
       @warriors = warriors
       @turns = 0
       @state = "Только началась"
+      @log_file = File.new("#{File.dirname(__FILE__)}/../logs/battle_#{next_log_count}.log", 'w')
     end
 
     def turn
       @warriors.each do |w|
         next if w.hp < 0
-        puts "\n#{w.name} атакует:"
-        puts '-' * 35
+        logger "\n#{w.name} атакует:"
+        logger '-' * 35
         @warriors.each do |j|
           next if j == w || j.hp < 0
-          print "#{w.name} бьет #{j.name} | "
+          buf = "#{w.name} бьет #{j.name} | "
           j.hit(w.dmg)
-          puts "Урон #{w.dmg}, #{j.name}: #{j.hp <= 0 ? 'убит' : j.hp.to_s + ' хп'}"
+          buf << "Урон #{w.dmg}, #{j.name}: #{j.hp <= 0 ? 'убит' : j.hp.to_s + ' хп'}"
+          logger buf
         end
       end
       @turns += 1
@@ -43,6 +45,14 @@ module WildBattleGame
     def eval_state
       wars = @warriors.select { |w| w.hp > 0 }
       @state = wars.count > 1 ? "В разгаре" : "#{wars[0].name} - победил"
+    end
+
+    def logger(msg)
+      @state[-1..-7] != 'победил' ? @log_file.write("#{msg}\n") : @log_file.close
+    end
+
+    def next_log_count
+      Dir["#{File.dirname(__FILE__)}/../logs/battle_*.log"].map { |f| File.basename(f)[/\d+/].to_i }.max.next
     end
   end
 end
